19ae3cf2e5f578d9cfbfe01ef12840f292ca1425
emjshrx <emjshrx@gmail.com>
2026-07-22 18:58:45 +0200

    Revert "feat: simplicity primitives"

--- rust/src/contracts/mod.rs
+++
@@ -1,4 +0,0 @@
-//! Internal Simplicity covenant primitives (not exposed via FRB / Dart).
-
-mod lwk_error;
-pub mod simplicity;
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(unexpected_cfgs)]
 
 pub mod api;
-mod contracts;
 #[cfg(not(feature = "bull_sdk"))]
 mod frb_generated;
